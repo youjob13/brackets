6c1cbfbb0089c090987735bfe155b5131a2fffa1
Danil Rodionow <danil.rodionow13@gmail.com>
2021-02-02 11:40:23 +0300

    init: implement function about check brackets sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let strArr = str.split("");
+    let index = -1;
+    bracketsConfig.forEach((item) => {
+        if (str.indexOf(item.join("")) >= 0) {
+            index = str.indexOf(item.join(""));
+        }
+    });
+    if (index >= 0) {
+        strArr = [...strArr.slice(0, index), ...strArr.slice(index + 2)];
+        return check(strArr.join(""), bracketsConfig);
+    } else {
+        return !str ? true : false;
+    }
+};
